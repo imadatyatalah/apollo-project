31b931a07a3b7833a111438fcffb0a4c53abb06b
Imad Atyat-Allah <imadatyatalah@gmail.com>
2022-04-05 13:00:07 +0000

    fix: only fetch countries when ClientOnly component is mounted

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,49 +1,13 @@
 import React from "react";
 
-import { useQuery, gql } from "@apollo/client";
-
-import ClientOnly from "../components/clientOnly";
-
-const QUERY = gql`
-  query Countries {
-    countries {
-      code
-      name
-      emoji
-    }
-  }
-`;
-
-type Country = {
-  code: string;
-  name: string;
-  emoji: string;
-};
+import ClientOnly from "../components/ClientOnly";
+import Countries from "../components/Countries";
 
 const Home = () => {
-  const { data, error, loading } = useQuery<{ countries: Country[] }>(QUERY);
-
-  if (loading) {
-    return <h2>Loading...</h2>;
-  }
-
-  if (error) {
-    return <h2>Oooops, Something went wrong!</h2>;
-  }
-
-  const countries = data.countries.slice(0, 4);
-
   return (
     <section>
       <ClientOnly>
-        {countries.map((country) => (
-          <div key={country.code} className="">
-            <h3>{country.name}</h3>
-            <p>
-              {country.code} - {country.emoji}
-            </p>
-          </div>
-        ))}
+        <Countries />
       </ClientOnly>
     </section>
   );
